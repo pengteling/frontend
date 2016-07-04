dd0ef5074a4a27cb5195057dbdc100a558778f92
pengteling <pengteling@gmail.com>
2016-07-04 15:46:49 +0800

    2

---
+++ js/comm.js
@@ -0,0 +1,10 @@
+$(function() {
+$("#meizu-header-link-product-meilan").hover(function() {
+    $("#meizu-header-sub-meilan").show().addClass("show");
+
+}, function() {
+    $("#meizu-header-sub-meilan").hide().removeClass("show");
+
+}
+)
+})
